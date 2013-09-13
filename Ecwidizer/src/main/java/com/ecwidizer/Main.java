a38942df4e248bc1f2b8e165d0eadc77067705b0
Igor Kurguzov <igor@ecwid.com>
2013-09-13 21:56:39 +0400

    пара конфиг-файлов

--- Ecwidizer/src/main/java/com/ecwidizer/Main.java
+++ Ecwidizer/src/main/java/com/ecwidizer/Main.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 
 public class Main extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
