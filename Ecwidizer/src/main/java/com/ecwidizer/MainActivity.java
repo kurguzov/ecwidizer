b5f5f91a609c039d6fa2883bb9fd153c94b0007a
Igor Kurguzov <igor@ecwid.com>
2014-10-25 19:35:18 +0400

    переименовал xml

--- Ecwidizer/src/main/java/com/ecwidizer/MainActivity.java
+++ Ecwidizer/src/main/java/com/ecwidizer/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.main_activity);
         setBusy(false);
 
 		initApplication();
